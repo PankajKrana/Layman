9340f39e5b1af5801c5a06ebeb036ffac7124cf5
Pankaj Kumar Rana <pankajkumarrana0611@gmail.com>
2026-04-01 22:39:39 +0530

    Initial Commit

---
+++ Layman/LaymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApp.swift
+//  Layman
+//
+//  Created by Pankaj Kumar Rana on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
